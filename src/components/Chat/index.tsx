2e5d24250265cf97631f3816287240455ce58234
cunguyendev <cunguyen.dev@gmail.com>
2025-04-07 07:20:07 +0700

    Change width of chat content

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -10,7 +10,7 @@ interface ChatProps {
 
 export const Chat = ({ messages }: ChatProps) => {
   return (
-    <div className="max-w-[425px] w-full mx-auto flex-1 overflow-y-auto p-4 rounded-lg">
+    <div className="max-w-[768px] w-full mx-auto flex-1 overflow-y-auto p-4 rounded-lg">
       <div className="flex flex-col gap-3">
         {messages.map(({ message, isYou }) => {
           return (
