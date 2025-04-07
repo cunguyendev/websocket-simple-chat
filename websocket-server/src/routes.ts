a9e79eb535860e24a53c6c1851864772ad218c2f
cunguyendev <cunguyen.dev@gmail.com>
2025-04-07 11:02:44 +0700

    Add default response

--- websocket-server/src/routes.ts
+++ websocket-server/src/routes.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/', (_, res) => {
+  res.status(200).send('WebSocket - Simple chat');
+});
+
 router.get('/health', (_, res) => {
   res.status(200).send('OK');
 });
